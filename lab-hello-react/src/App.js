e57a5877a01e6e9a1ab98eb2cd5909f4eaf40555
Dani BL <danibachsl@protonmail.com>
2022-07-11 17:27:03 +0200

    done

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import logo from './images/ironhack-logo-xs.png';
 import logo2 from './images/menu-top-xs.png';
+import logo3 from './images/icon1.png';
+import logo4 from './images/icon2.png';
+import logo5 from './images/icon3.png';
+import logo6 from './images/icon4.png';
 
 function App() {
   return (
@@ -24,6 +28,40 @@ function App() {
 
         <div className="whiteButton">
           <button style={{ fontSize: "17px" }}><b>Awesome!</b></button>
+        </div>
+      </div>
+
+      <div className="imagesDiv">
+        <div>
+          <div><img src={logo3} alt="Logo" /></div>
+          <p style={{ fontSize: "20px" }}><b>Declarative</b></p>
+          <p>React makes it</p>
+          <p>painless to create</p>
+          <p>interactive UIs.</p>
+        </div>
+
+        <div>
+          <div><img src={logo4} alt="Logo" /></div>
+          <p style={{ fontSize: "20px" }}><b>Components</b></p>
+          <p>Build encapsulated</p>
+          <p>components that</p>
+          <p>manage their state.</p>
+        </div>
+
+        <div>
+          <div><img src={logo5} alt="Logo" /></div>
+          <p style={{ fontSize: "20px" }}><b>Single-Way</b></p>
+          <p>A set of immutable</p>
+          <p>values are passed to</p>
+          <p>the components.</p>
+        </div>
+
+        <div>
+          <div><img src={logo6} alt="Logo" /></div>
+          <p style={{ fontSize: "20px" }}><b>JSX</b></p>
+          <p>Statically-typed,</p>
+          <p>designed to run on</p>
+          <p>modern browsers.</p>
         </div>
       </div>
     </div>
